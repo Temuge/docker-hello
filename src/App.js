8c95f04ac707abcb756fc8a040b05a82605b8be3
Temuge Enkhbaatar <temuge@Temuges-MacBook-Pro.local>
2022-01-06 15:18:42 -0500

    change app text on feature branch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           [8] Edit <code>src/App.js</code> and save to reload.
+          Feature branch 
         </p>
         <a
           className="App-link"
